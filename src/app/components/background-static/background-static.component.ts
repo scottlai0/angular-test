c2fad66a9399de74ec73f03d5595cbf5024ff989
scottlai0 <scottlai96@hotmail.com>
2023-06-06 05:44:29 +0800

    configurable static background

--- src/app/components/background-static/background-static.component.ts
+++ src/app/components/background-static/background-static.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'background-static',
   templateUrl: './background-static.component.html',
   styleUrls: ['./background-static.component.scss']
 })
-export class BackgroundStaticComponent {
+export class BackgroundStaticComponent implements OnInit {
+  @Input() id?: String;
+  @Input() backgroundUrl?: String;
+  @Input() padding?: String;
+  @ViewChild('content') content: any;
 
+  ngOnInit(){}
+
+  ngAfterViewInit(){
+    if (!this.id) this.id = uuid();
+
+    let styles: String = '';
+    styles = this.backgroundUrl ? styles += `background: url(${this.backgroundUrl}) no-repeat center center fixed; background-size: cover;` : '';
+    styles = this.padding ? styles += `padding: ${this.padding};` : styles += `padding: 3rem;`;
+
+    this.content.nativeElement.setAttribute('style', styles);
+    //console.log(this.content.nativeElement);
+  }
+  
 }
+
+
